2481757d2570398e849ea0c0b51a6c457a4234b5
prasad <prasad@prasad-PC>
2021-12-19 19:59:14 +0530

    ok modify the code

--- Velocity_Aug 21/src/Logical_program/Armstrong_Number1.java
+++ Velocity_Aug 21/src/Logical_program/Armstrong_Number1.java
@@ -11,10 +11,22 @@ public class Armstrong_Number1
 		Scanner s=new Scanner(System.in);
 		int num1=s.nextInt();
 		int num2=0;
+		
 		for(int i=num1;i>0;i=i/10)
 		{
-			
+			int rem=i%10;
+			num2=num2+(rem*rem*rem);
 		}
+		if(num1==num2)
+			
+		{
+			System.out.println("given number is palindrome");
+		}
+		
+		else
+	    {
+		 System.out.println("given number is not palindrome");
+	    }
 		
 	}
 }
